5f619b45d12495b2d68b9be523f02a6af1d39f1b
Jordy <jordiramirez98>
2026-09-18 00:43:54 -0500

    feat: add proveedores module

---
+++ lib/Proveedores/models/proveedor.dart
@@ -0,0 +1,17 @@
+class Proveedor {
+  final String id;
+  String nit;
+  String nombre;
+  String contacto;
+  String telefono;
+  String estado;
+
+  Proveedor({
+    required this.id,
+    required this.nit,
+    required this.nombre,
+    required this.contacto,
+    required this.telefono,
+    required this.estado,
+  });
+}
